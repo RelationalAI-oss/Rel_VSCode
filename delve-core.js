6322c1971b1e204bc19a055476cab6a607875347
Mohammad Dashti <mdashti@gmail.com>
2021-02-01 11:10:34 -0800

    Added support for `@auto_number`

--- delve-core.js
+++ delve-core.js
@@ -3,6 +3,7 @@ exports.map = {
   "bound": "    Bound Declarations\n\nBound declarations define the upper-bound of a relation that is persisted in the database (and is not computed by a definition). This is similar to a SQL table, or in database theory it is known as an EDB relation.\n\nIt is not possible for the EDB relation to contain any tuple that is not in the upper-bound.\n\n- `bound parent = a, b: Person(a) and Person(b)`\n- `bound parent(a, b) = Person(a) and Person(b)`\n- `bound parent = Person, Person`\n- `bound orderdate(x, y) = Int(x) and Date(y)`",
   "implies": "    Implication\n\n`e1 implies e2` is defined as `(not e1) or (e1 and e2)`\n\nExample:\n\n- `ic no_self_edge(x,y) { edge(x,y) implies x != y }`",
   "@inline": "    Inline Annotation\n\n`@inline` specfies deferred evaluation. When referenced later in the definition of another relation, it will be evaluated at that time.",
+  "@auto_number": "    Auto-Number Annotation\n\n`@auto_number` van be used on entity definitions and specifies the usage of auto-numbered entities.",
   "@inspect": "    Inspection\n\nReturns information about a definition.\n\n`@inspect def netsales[x, y, z] = sales[x, y, z] - returns[x, y, z]`",
   "@function": "Deprecated",
 
@@ -24,7 +25,7 @@ exports.map = {
 
   "from": "    From Expressions\n\nFrom expressions are similar to relational abstractions, but the variables introduced by the binding are not part of the resulting value. The arity of the expression is entirely determined by the expression. Examples:\n\n- `x+1 from x in {1; 2; 3}` results in `{2; 3; 4}`\n- `x, x+1 for x in {1; 2; 3}` results in `{(1,1,2); (2,2,3); (3,3,4)}`\n\nCompare to `for`:\n\n- `x+1 for x in {1; 2; 3}` results in `{(1,2); (2,3); (3,4)}`\n- `x, x+1 for x in {1; 2; 3}` results in `{(1,1,2); (2,2,3); (3,3,4)}`",
   "for": "    Aggregation\n\n- `x+1 for x in {1; 2; 3}` results in `{(1,2); (2,3); (3,4)}`\n- `x, x+1 for x in {1; 2; 3}` results in `{(1,1,2); (2,2,3); (3,3,4)}`\n\nCompare to `from`:\n\n- `x+1 from x in {1; 2; 3}` results in `{2; 3; 4}`\n- `x, x+1 for x in {1; 2; 3}` results in `{(1,1,2); (2,2,3); (3,3,4)}`",
-  
+
   "eq": "    eq(x,y)\n\nsame as ```=``` but different than ```equal(R,S)```",
   "neq": "    neq(x,y)\n\nsame as !=",
   "gt": "    gt(x,y)\n\nsame as >",
